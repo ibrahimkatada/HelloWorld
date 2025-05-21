cb91b5eba0bbc67a174a5d424cc96e9144f474a5
ibrahimkatada <ibrahimkatada@gmail.com>
2025-05-21 16:25:15 +0300

    1

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var GreetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        greetingLabel.isHidden.toggle()
+        GreetingButton.layer.cornerRadius = 10
 
     }
-
+    @IBAction func greetingButtonDidTapped(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        sender.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+    }
+    
 
 }
 
